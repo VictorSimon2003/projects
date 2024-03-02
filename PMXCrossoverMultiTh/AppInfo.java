89bb1c5685968b8435d04d7625f807bb720df62f
VictorSimon2003 <161367786+VictorSimon2003@users.noreply.github.com>
2024-03-02 23:12:38 +0200

    Update AppInfo.java

--- PMXCrossoverMultiTh/AppInfo.java
+++ PMXCrossoverMultiTh/AppInfo.java
@@ -1,1 +1,12 @@
+public class AppInfo
+{
+    static int runTime, totalNoPopulation, noThreads, matrixSize, shortesPath, mergerFrequency, noIterations ;
+    static float mergerFrequencyFloat;
+    static int []threadNoPopulation;
+    static PMXThread []PMXProcessingThreads;
+    static int [][]matrix;
+    static double mutationProbability;
+    static boolean killThreads, mergePopulation;
+
+}
 
